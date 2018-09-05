e3a8cb5bfc8d101597c435e630a2822821d9e076
opringle <ojapringle@gmail.com>
2018-09-05 15:06:01 -0700

    building with pre mxnet version

--- build_custom_image.sh
+++ build_custom_image.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-cd docker/1.2.1/base
-docker build -t mxnet-base:1.2.1-gpu-py3 -f Dockerfile.gpu .
+cd docker/1.3.1/base
+docker build -t mxnet-base:1.3.0-gpu-py3 -f Dockerfile.gpu .
 cd ../../../
 python setup.py sdist
-cp dist/sagemaker_mxnet_container-1.0.0.tar.gz docker/1.2.1/final
-cd docker/1.2.1/final
-wget https://files.pythonhosted.org/packages/bb/53/5d33f71c5224a676112679458714eb728f6db8cae15f39fcdf27226f6e41/mxnet-1.2.1.post1-py2.py3-none-manylinux1_x86_64.whl
-docker build -t mxnet-gluonnlp-pandas-cu92:1.2.1-gpu-py3 --build-arg py_version=3 --build-arg framework_installable=mxnet-1.2.1.post1-py2.py3-none-manylinux1_x86_64.whl -f Dockerfile-gluonnlp-pandas.gpu .
+cp dist/sagemaker_mxnet_container-1.0.0.tar.gz docker/1.3.0/final
+cd docker/1.3.0/final
+wget https://files.pythonhosted.org/packages/e1/73/a6c15322ad509ebd156491a9e0342713a1e6d7cca6a94a110daf4f963db4/mxnet-1.3.0b20180906-py2.py3-none-win_amd64.whl
+docker build -t mxnet-pre-gluonnlp-pandas-cu92:1.2.1-gpu-py3 --build-arg py_version=3 --build-arg framework_installable=mxnet-1.3.0b20180906-py2.py3-none-win_amd64.whl -f Dockerfile-gluonnlp-pandas.gpu .
